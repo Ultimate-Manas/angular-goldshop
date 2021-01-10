e0cf945e1405f5ad1fe8702915cd445523b2a2ed
ultimatemanas <manaspattanaik777@gmail.com>
2021-01-10 21:43:38 +0530

    Add logout function

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,13 +1,37 @@
 import { Component, OnInit } from "@angular/core";
-
+import { RouteService } from "./../route.service";
 @Component({
   selector: "app-layout",
   templateUrl: "./layout.component.html",
   styleUrls: ["./layout.component.css"]
 })
 export class LayoutComponent implements OnInit {
-  isLoggedIn: Boolean = false;
-  constructor() {}
+  isLoggedIn: Boolean = true;
 
-  ngOnInit() {}
+  constructor(private routeService: RouteService) {}
+
+  ngOnInit() {
+    this.isLoggedIn = this.routeService.IsLoggedIn();
+  }
+
+  public logout() {
+    this.routeService.Logout();
+  }
+
+  ngAfterViewInit(): void {
+    var self = this;
+    setTimeout(function() {
+      console.log("view init");
+      self.isLoggedIn = self.routeService.IsLoggedIn();
+    }, 1);
+  }
+
+  ngAfterViewChecked() {
+    var self = this;
+    setTimeout(function() {
+      console.log("view checked");
+      self.isLoggedIn = self.routeService.IsLoggedIn();
+      console.log(self.isLoggedIn);
+    }, 1);
+  }
 }
--- src/app/route.service.ts
+++ src/app/route.service.ts
@@ -1,10 +1,12 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
+import { Router } from "@angular/router";
 
 @Injectable()
 export class RouteService {
   isPriv: boolean = false;
-  constructor(private http: HttpClient) {}
+  isLoggedIn: boolean = false;
+  constructor(private http: HttpClient, private route: Router) {}
 
   public validateAuth(userName, userPassword) {
     this.http
@@ -21,11 +23,25 @@ export class RouteService {
   }
 
   public setAuth(tokenValue: string, isPriv: boolean) {
+    this.isLoggedIn = true;
     this.isPriv = isPriv;
     localStorage.setItem("token", tokenValue);
   }
 
   public isPrivUser(): boolean {
     return this.isPriv;
+  }
+
+  public IsLoggedIn() {
+    return this.isLoggedIn;
+  }
+
+  public Logout() {
+    if (confirm("Are you sure to logout")) {
+      this.isPriv = false;
+      this.isLoggedIn = false;
+      localStorage.removeItem("token");
+      this.route.navigate(["login"]);
+    }
   }
 }
